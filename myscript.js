0ac6cbf16f8f1e93fe07d37847dfda60bf8f0c8f
Michał Grablowski <michalgra25@gmail.com>
2022-11-13 18:31:14 +0100

    fixes

--- myscript.js
+++ myscript.js
@@ -67,102 +67,93 @@ function palindrome(var1) {
 }
 
 function findSndLargest(array) {
-	let max = array[0];
 	let temp = array[0];
 	let id = 0;
 	if (typeof temp == 'string') {
 		temp = temp.length;
 	}
-	if (typeof max == 'string') {
-		max = max.length;
-	}
+	let max = temp;
 
+	// max
 	for (let index = 0; index < array.length; index++) {
 		let element = array[index];
-		if (typeof element == 'string') {
-			element = element.length;
-		}
+		if (typeof element == 'string') element = element.length;
 		if (max < element) max = element;
 	}
 
+	// snd max
 	for (let index = 0; index < array.length; index++) {
 		let element = array[index];
-		if (typeof element == 'string') {
-			element = element.length;
-		}
+		if (typeof element == 'string') element = element.length;
 		if (temp < element && element < max) {
 			temp = element;
 			id = index;
 		}
 	}
 
-	if (typeof array[id] == 'string') {
-		return array[id];
-	} else {
-		return temp;
-	}
+	return typeof array[id] == 'string' ? array[id] : temp;
 }
 
-function handleAnagramForm(form) {
-	let word1 = form.inputbox3.value;
-	let word2 = form.inputbox4.value;
-	document.getElementById('anagramResult').innerHTML = anagram(word1, word2);
-}
+// function handleAnagramForm(form) {
+// 	let word1 = form.inputbox3.value;
+// 	let word2 = form.inputbox4.value;
+// 	document.getElementById('anagramResult').innerHTML = anagram(word1, word2);
+// }
 
-function bubbleSort(arr) {
-	for (var i = 0; i < arr.length; i++) {
-		for (var j = 0; j < arr.length - i - 1; j++) {
-			if (arr[j] > arr[j + 1]) {
-				var temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-			}
-		}
-	}
-	return arr;
-}
+// function bubbleSort(arr) {
+// 	for (var i = 0; i < arr.length; i++) {
+// 		for (var j = 0; j < arr.length - i - 1; j++) {
+// 			if (arr[j] > arr[j + 1]) {
+// 				var temp = arr[j];
+// 				arr[j] = arr[j + 1];
+// 				arr[j + 1] = temp;
+// 			}
+// 		}
+// 	}
+// 	return arr;
+// }
 
-function anagram(var1, var2) {
-	let word1 = [];
-	let word2 = [];
-	let flag = true;
+// function anagram(var1, var2) {
+// 	let word1 = [];
+// 	let word2 = [];
+// 	let flag = true;
 
-	word1ASCII = [];
-	word2ASCII = [];
+// 	word1ASCII = [];
+// 	word2ASCII = [];
 
-	word1 = var1.split('');
-	word2 = var2.split('');
+// 	word1 = var1.split('');
+// 	word2 = var2.split('');
 
-	if (word1.length == word2.length) {
-		for (let i = 0; i < word1.length; i++) {
-			word1ASCII[i] = word1[i].charCodeAt(0);
-		}
+// 	if (word1.length == word2.length) {
+// 		for (let i = 0; i < word1.length; i++) {
+// 			word1ASCII[i] = word1[i].charCodeAt(0);
+// 		}
 
-		for (let i = 0; i < word2.length; i++) {
-			word2ASCII[i] = word2[i].charCodeAt(0);
-		}
+// 		for (let i = 0; i < word2.length; i++) {
+// 			word2ASCII[i] = word2[i].charCodeAt(0);
+// 		}
 
-		word1ASCII = bubbleSort(word1ASCII);
-		word2ASCII = bubbleSort(word2ASCII);
+// 		word1ASCII = bubbleSort(word1ASCII);
+// 		word2ASCII = bubbleSort(word2ASCII);
 
-		console.log(word1ASCII);
-		console.log(word2ASCII);
+// 		console.log(word1ASCII);
+// 		console.log(word2ASCII);
 
-		for (let i = 0; i < word1ASCII.length; i++) {
-			if (word1ASCII[i] != word2ASCII[i]) {
-				flag = false;
-				break;
-			}
-		}
-	} else {
-		return false;
-	}
-	return flag;
-}
+// 		for (let i = 0; i < word1ASCII.length; i++) {
+// 			if (word1ASCII[i] != word2ASCII[i]) {
+// 				flag = false;
+// 				break;
+// 			}
+// 		}
+// 	} else {
+// 		return false;
+// 	}
+// 	return flag;
+// }
 
-console.log(anagram('siema', 'siemb'));
-console.log('a'.charCodeAt(0));
-console.log('b'.charCodeAt(0));
+// console.log(anagram('siema', 'siemb'));
+// console.log('a'.charCodeAt(0));
+// console.log('b'.charCodeAt(0));
 
 console.log('snd largest: ');
 console.log(findSndLargest([1, 5, 10, 15]));
